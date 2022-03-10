1588f0073f3667963f6d1e6ab00556d2afef1c15
Pedro Henrique <pedro.admoura@gmail.com>
2022-03-10 16:14:28 -0300

    Requisito 2

---
+++ src/pages/Search.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
