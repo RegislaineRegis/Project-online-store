7883e2e631dbd83be508ab3bb6bb3678ba837cbf
Débora Rodrigues Serra <94383038+DeboraSerra@users.noreply.github.com>
2022-03-10 16:32:12 -0300

    Update src/pages/Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,7 +4,7 @@ export default class Search extends Component {
   render() {
     return (
       <div>
-        <input type="text" />
+        <input type="text" name="query" placeholder="Escreva um termo para sua pesquisa" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
