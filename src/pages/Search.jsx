66b5015a2f4d628bad8ef0dc8cc364233b936876
Débora Serra <debora.r.serra@gmail.com>
2022-03-10 17:25:31 -0300

    Adiciona requisito 4

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
+import * as api from '../services/api';
+import CategoriesButtons from '../components/CategoryButtons';
+import Loading from '../components/Loading';
+import '../styles/Buttons.css';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.requestCategories();
+  }
+
+  requestCategories = async () => {
+    const categories = await api.getCategories();
+    this.setState({
+      categories,
+      loading: false,
+    });
+  }
+
+  handleClick = () => {
+
+  }
+
   render() {
+    const { categories, loading } = this.state;
     return (
       <div>
-        <input type="text" name="query" placeholder="Escreva um termo para sua pesquisa" />
+        <section className="buttons-sect">
+          {loading ? <Loading /> : (
+            categories.map((cat) => (
+              <CategoriesButtons
+                key={ cat.id }
+                category={ cat.name }
+                handleClick={ this.handleClick }
+              />
+            ))
+          )}
+        </section>
+        <input
+          type="text"
+          name="query"
+          placeholder="Escreva um termo para sua pesquisa"
+        />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
