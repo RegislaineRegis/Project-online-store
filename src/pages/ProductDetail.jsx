aa97f44d480c6a7da630f769c4c9902dfae1c2ba
PedroHOM16 <89684189+PedroHOM16@users.noreply.github.com>
2022-03-11 14:20:57 -0300

    Update src/pages/ProductDetail.jsx
    Co-authored-by: Débora Rodrigues Serra <94383038+DeboraSerra@users.noreply.github.com>

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -26,7 +26,7 @@ export default class ProductDetail extends Component {
   componentDidMount() {
     // console.log(this.props)
     const { match: { params: { id } } } = this.props;
-    this.getProduct(id)
+    this.getProduct(id);
   }
 
   getProduct = async (id) => {
