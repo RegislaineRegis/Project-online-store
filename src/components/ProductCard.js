206f68117f6c58f9dc8b1e6f1b901db724f6c9cb
RegislaineRegis <nanafidelis@gmail.com>
2022-03-11 09:43:37 -0300

    realizaçao do requisito 15

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -11,7 +11,7 @@ class ProductCard extends React.Component {
         <h2>{title}</h2>
         <img src={ thumbnail } alt={ title } />
         <p>{`R$ ${price}`}</p>
-        {freeShipping && <p data-testid="free-shipping">Frete grátis</p>} 
+        {freeShipping && <div data-testid="free-shipping">Frete grátis</div >} 
       </section>
     );
   }
