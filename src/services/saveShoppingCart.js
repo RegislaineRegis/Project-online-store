f82e97fe7e9aec274bc5493c6954bf272d5008d3
RegislaineRegis <nanafidelis@gmail.com>
2022-03-11 17:16:21 -0300

    realização do requisito 9

--- src/services/saveShoppingCart.js
+++ src/services/saveShoppingCart.js
@@ -6,18 +6,17 @@ if (!localStorage.getItem(CART_KEY)) {
 
 export const getShoppingCart = () => {
   const getCart = JSON.parse(localStorage.getItem(CART_KEY));
-  if (getCart.length === 0) return 'Seu carrinho está vazio';
   return getCart;
-}
+};
 
 export const addItem = (item) => {
   const cart = JSON.parse(localStorage.getItem(CART_KEY));
   const newCart = [...cart, item];
   localStorage.setItem(CART_KEY, JSON.stringify(newCart));
-}
+};
 
 export const removeItem = (item) => {
   const cart = JSON.parse(localStorage.getItem(CART_KEY));
   const newCart = cart.filter((cartItem) => cartItem.id !== item.id);
   localStorage.setItem(CART_KEY, JSON.stringify(newCart));
-}
+};
