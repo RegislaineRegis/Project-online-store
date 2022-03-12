f0e1b405e8ed4c5e707fd764b2cec4daf24e7702
Débora Serra <debora.r.serra@gmail.com>
2022-03-12 09:49:54 -0300

    Corriige bug do requisito10

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import * as cartFunctions from '../services/saveShoppingCart';
+// import * as cartFunctions from '../services/saveShoppingCart';
 
 class CartItem extends Component {
   constructor() {
@@ -19,28 +19,43 @@ class CartItem extends Component {
     this.setState({ title, thumbnail, price, quantity, id });
   }
 
-  addQuantity = () => {
-    // const { quantity } = this.state;
-    this.setState((prev) => ({ quantity: prev.quantity + 1 }), () => {
-      const { id, title, quantity, price, thumbnail } = this.state;
-      cartFunctions.updateItem({ id, title, quantity, price, thumbnail });
-    });
-  }
+  // removeQuantity = () => {
+  //   this.setState((prev) => ({
+  //     quantity: prev.quantity > 1 ? prev.quantity - 1 : prev.quantity,
+  //   }), () => {
+  //     const { id, title, quantity, price, thumbnail } = this.state;
+  //     cartFunctions.updateItem({ id, title, quantity, price, thumbnail });
+  //   });
+  // }
+
+  // addQuantity = () => {
+  //   // const { quantity } = this.state;
+  //   this.setState((prev) => ({ quantity: prev.quantity + 1 }), () => {
+  //     const { id, title, quantity, price, thumbnail } = this.state;
+  //     cartFunctions.updateItem({ id, title, quantity, price, thumbnail });
+  //   });
+  // }
+
+  // deleteProd = () => {
+  //   const { title, thumbnail, price, quantity, id } = this.state;
+  //   cartFunctions.removeItem({ title, thumbnail, price, quantity, id });
+  // }
 
   render() {
     const { product: { title, thumbnail, price, quantity, id } } = this.props;
+    const total = price * quantity;
     return (
       <section id={ id }>
         <img src={ thumbnail } alt={ title } />
-        <h3 data-testid="shopping-cart-product-name">{ title }</h3>
-        <p>{ `R$ ${price.toFixed(2).replace(/\./gm, ',')}` }</p>
-        <p data-testid="shopping-cart-product-quantity">{ `Un: ${quantity}` }</p>
-        <button
-          type="button"
-          onClick={ this.addQuantity }
-        >
-          Adiciona mais um +.
-        </button>
+        <h3 data-testid="shopping-cart-product-name">
+          { title }
+        </h3>
+        <p>
+          { `Preço unid: R$ ${price.toFixed(2).replace(/\./gm, ',')}` }
+        </p>
+        <p>
+          {`Preço total: R$ ${total.toFixed(2).replace(/\./gm, ',')}`}
+        </p>
       </section>
     );
   }
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as cartFunctions from '../services/saveShoppingCart';
 import CartItem from '../components/CartItem';
+import Loading from '../components/Loading';
 
 export default class ShoppingCart extends Component {
   constructor() {
     super();
     this.state = {
       products: [],
+      loading: false,
     };
   }
 
@@ -16,18 +18,88 @@ export default class ShoppingCart extends Component {
     this.setState({ products });
   }
 
+  removeQuantity = (product) => {
+    let { id, title, price, thumbnail, quantity } = product;
+    if (quantity === 0) quantity = 0;
+    else quantity = quantity - 1;
+    const newProd = { id, title, price, thumbnail, quantity };
+    if(quantity === 0) {
+      alert('Esse produto será removido do seu carrinho');
+      cartFunctions.removeItem(newProd);
+    }
+    else cartFunctions.updateItem(newProd);
+    this.setState({ loading: true }, () => {
+      this.setState({
+        products: cartFunctions.getShoppingCart(),
+        loading: false })
+    })
+    
+    
+  }
+
+  addQuantity = (product) => {
+    // const { products } = this.state;
+    let { id, title, price, thumbnail, quantity } = product;
+    quantity = quantity + 1;
+    const newProd = { id, title, price, thumbnail, quantity };
+    cartFunctions.updateItem(newProd);
+    this.setState({ loading: true }, () => {
+      this.setState({ products: cartFunctions.getShoppingCart(),
+        loading: false })
+    })
+  }
+
+  deleteProd = (product) => {
+    cartFunctions.removeItem(product);
+    this.setState({ loading: true }, () => {
+      this.setState({ products: cartFunctions.getShoppingCart(),
+        loading: false });
+    })
+  }
+
   render() {
-    const { products } = this.state;
+    const { products, loading } = this.state;
+    const total = products.reduce((acc, { price, quantity }) => acc + (price * quantity), 0);
     return (
       <section>
         <Link to="/"> HOME </Link>
-        {products.length === 0 ? (
-          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-        ) : (
-          products.map((product) => (
-            <CartItem product={ product } key={ product.id } />
-          ))
-        ) }
+        {loading ? <Loading /> : (
+          products.length === 0 ? (
+            <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+          ) : (
+            <section>
+              {products.map((product) => (
+                <section key={ product.id }>
+                  <button
+                    type="button"
+                    onClick={ () => this.deleteProd(product) }
+                  >
+                    X
+                  </button>
+                  <CartItem
+                    product={ product }
+                  />
+                  <button
+                    type="button"
+                    onClick={ () => this.removeQuantity(product) }
+                  >
+                    -
+                  </button>
+                  <p data-testid="shopping-cart-product-quantity">
+                    { `Un: ${product.quantity}` }
+                  </p>
+                  <button
+                    type="button"
+                    onClick={ () => this.addQuantity(product) }
+                  >
+                    +
+                  </button>
+                </section>
+              ))}
+              <p>{`Valor final: R$ ${total.toFixed(2).replace(/\./gm, ',')}`}</p>
+            </section>
+          )
+        )}
       </section>
     );
   }
